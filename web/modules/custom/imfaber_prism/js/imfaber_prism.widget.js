dcc93f33eb33b8be2c7e4d1225ebda4cdede3e09
Fabrizio Meinero <imfaberdev@gmail.com>
2018-03-05 20:06:39 +0000

    Fix ace editor in in admin forms

--- web/modules/custom/imfaber_prism/js/imfaber_prism.widget.js
+++ web/modules/custom/imfaber_prism/js/imfaber_prism.widget.js
@@ -9,7 +9,7 @@
   Drupal.behaviors.imfaberPrismWidget = {
     attach: function (context, settings) {
 
-      $('.field--widget-imfaber-text-prism').each(function (i, elem) {
+      $('.field--widget-imfaber-text-prism .text-format-wrapper').each(function (i, elem) {
         var $textarea    = $('.imfaber-prism-widget__textarea', $(elem)),
             $select      = $('.imfaber-prism-widget__lang-select', $(elem)),
             textarea_val = $textarea.val();
